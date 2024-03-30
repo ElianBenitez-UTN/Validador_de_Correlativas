040ec35c8efcd3fd2a2b320277d1064b0888262d
Fernando Elian Benitez <febenitez@frba.utn.edu.ar>
2024-03-30 20:59:07 -0300

    Subo el esqueleto del programa
    Class Materia
    Class Inscripcion
    Class Alumno

---
+++ src/main/java/facultad/Inscripcion.java
@@ -0,0 +1,12 @@
+package facultad;
+
+import java.util.List;
+
+public class Inscripcion {
+    private List<Materia> materias;
+    private Alumno alumno;
+
+    public Boolean aprobada() {
+        return true;
+    }
+}
