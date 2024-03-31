541d6679d6e05971b00c829e73c9ca7c80db0b42
Fernando Elian Benitez <febenitez@frba.utn.edu.ar>
2024-03-30 21:09:16 -0300

    Completo Class Materia
    Agrego sus metodos y atributos iniciales.

--- src/main/java/facultad/Materia.java
+++ src/main/java/facultad/Materia.java
@@ -1,5 +1,15 @@
 package facultad;
 
-public class Materia {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Materia {
+    private String nombre;
+
+    private List<Materia> correlativas;
+
+    public Materia(String nombre) {
+        this.nombre = nombre;
+        this.correlativas = new ArrayList<>();
+    }
 }
