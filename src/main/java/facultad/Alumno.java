075ea455e695950721397c0fd429fab4c35576fd
Fernando Elian Benitez <febenitez@frba.utn.edu.ar>
2024-03-30 21:07:19 -0300

    Dejo planteada la Clase Alumno
    Agrego atributos y metodos iniciales

--- src/main/java/facultad/Alumno.java
+++ src/main/java/facultad/Alumno.java
@@ -1,4 +1,15 @@
 package facultad;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Alumno {
+    private String nombre;
+
+    public List<Materia> materiasAprobadas;
+
+    public Alumno(String nombre) {
+        this.nombre = nombre;
+        this.materiasAprobadas = new ArrayList<>();
+    }
 }
--- src/main/java/facultad/Inscripcion.java
+++ src/main/java/facultad/Inscripcion.java
@@ -7,6 +7,7 @@ public class Inscripcion {
     private Alumno alumno;
 
     public Boolean aprobada() {
+        //TODO
         return true;
     }
 }
